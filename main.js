c6bc270c270d857c3916d8ea5327526c3ff7c0b9
FineBoy-001 <samuelamohjunior01@gmail.com>
2026-06-30 11:11:13 +0100

    added some new features

--- main.js
+++ main.js
@@ -16,3 +16,21 @@ navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-3-line");
 })
+
+const scrollRevealOption = {
+    distance: "60px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_container h1", {
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".header_content .section_description", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header_btn", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
